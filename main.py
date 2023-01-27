5de798634cd155c78f85a1b7dffcf66966aa6702
Andrew G <glazynovand@gmail.com>
2023-01-27 23:47:01 +0700

    fast api init

--- main.py
+++ main.py
@@ -1,2 +1,41 @@
-if __name__ == '__main__':
-    print('*************************** death star server ***************************')
+import time
+import random
+
+import logging
+import string
+
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
+app = FastAPI(
+    title="Death Star ACS",
+    description="Death Star ACS API",
+    version="0.0.1",
+    contact={
+        "name": "Andrew G.",
+        "url": "https://github.com/andrwnv/death_star_acs",
+        "email": "glazynovand@gmail.com",
+    },
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    logger.info("logging from the root logger")
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=2023)
