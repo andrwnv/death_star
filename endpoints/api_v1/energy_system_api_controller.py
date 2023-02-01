f07d0f54093e25faddcb65abcbeb8d362aa38cfe
Andrew G <glazynovand@gmail.com>
2023-02-01 21:31:35 +0300

    Formating fix

--- endpoints/api_v1/energy_system_api_controller.py
+++ endpoints/api_v1/energy_system_api_controller.py
@@ -11,13 +11,20 @@ class EnergySystemController(APIRouter):
 
         self.__manager = manager
 
-        self.add_api_route(path='/state', methods=['GET'], endpoint=self.get_state)
-        self.add_api_route(path='/state/{power_cell_name}', methods=['GET'], endpoint=self.get_cell_state)
-        self.add_api_route(path='/state/{power_cell_name}/colling_system', methods=['GET'], endpoint=self.get_colling_system_state)
-        self.add_api_route(path='/state/{power_cell_name}/magnet_system', methods=['GET'], endpoint=self.get_magnet_system_state)
-        self.add_api_route(path='/state/{power_cell_name}/plasma_heater', methods=['GET'], endpoint=self.get_plasma_heater_state)
-        self.add_api_route(path='/state/{power_cell_name}/vacuum_vessel', methods=['GET'], endpoint=self.get_vacuum_vessel_state)
-        self.add_api_route(path='/state/{power_cell_name}/fuel_storage', methods=['GET'], endpoint=self.get_fuel_storage_state)
+        self.add_api_route(path='/state',
+                           methods=['GET'], endpoint=self.get_state)
+        self.add_api_route(path='/state/{power_cell_name}',
+                           methods=['GET'], endpoint=self.get_cell_state)
+        self.add_api_route(path='/state/{power_cell_name}/colling_system',
+                           methods=['GET'], endpoint=self.get_colling_system_state)
+        self.add_api_route(path='/state/{power_cell_name}/magnet_system',
+                           methods=['GET'], endpoint=self.get_magnet_system_state)
+        self.add_api_route(path='/state/{power_cell_name}/plasma_heater',
+                           methods=['GET'], endpoint=self.get_plasma_heater_state)
+        self.add_api_route(path='/state/{power_cell_name}/vacuum_vessel',
+                           methods=['GET'], endpoint=self.get_vacuum_vessel_state)
+        self.add_api_route(path='/state/{power_cell_name}/fuel_storage',
+                           methods=['GET'], endpoint=self.get_fuel_storage_state)
 
     async def get_state(self):
         return {"TEST": []}
