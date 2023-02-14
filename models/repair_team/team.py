d5f7478909124efb5b478d3d3f68e0af98c7747a
Andrew G <glazynovand@gmail.com>
2023-02-14 20:31:39 +0700

    Generate repair teams (#3)

--- models/repair_team/team.py
+++ models/repair_team/team.py
@@ -1,7 +1,10 @@
 from typing import List
 
+import names
+
 from models.repair_team.employee import Employee
 from models.location import Location
+from models.repair_team.employee_grade import EmployeeGrade
 
 from utils.json_serializable import IJsonSerializable
 
@@ -11,7 +14,19 @@ class RepairTeam(IJsonSerializable):
         self.name = name
         self.is_busy = False
         self.current_location = Location.HOME
+
         self.employees = employees
+
+        if not len(employees):
+            for ix in range(15):
+                employee = Employee(name=names.get_full_name(), grade=EmployeeGrade.BEGINNER)
+                
+                if ix % 3 == 0:
+                    employee.grade = EmployeeGrade.ENGINEER
+                if ix % 6 == 0:
+                    employee.grade = EmployeeGrade.LEAD_ENGINEER
+                
+                self.employees.append(employee)
 
     name: str
     is_busy: bool
