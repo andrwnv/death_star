e65e2b6e149c11b7b7d525f9e9f9dbfe595f37ce
Andrew G <glazunov_ay@lemz-t.ru>
2023-02-16 17:03:24 +0700

    plasma heater: set numbers from doc

--- usecases/generators/plasma_heater_generator/default_generation_strategy.py
+++ usecases/generators/plasma_heater_generator/default_generation_strategy.py
@@ -5,6 +5,7 @@ from models.energy_system.plasma_heater.plasma_heater import PlasmaHeater
 from usecases.generators.base_generation_strategy import IGenerationStrategy
 from usecases.generators.plasma_heater_generator.constants import PlasmaHeaterDefaultParams
 
+
 class DefaultGenerationStrategy(IGenerationStrategy):
     def __init__(self, model: PlasmaHeater, name: str = f'PlasmaHeaterDefaultGenerator-{uuid.uuid4()}') -> None:
         super().__init__()
@@ -17,9 +18,13 @@ class DefaultGenerationStrategy(IGenerationStrategy):
             pass
 
         self.__model.alarm = False
-        
-        self.__model.temperature = random.gauss(
-            PlasmaHeaterDefaultParams.TEMPERATURE, PlasmaHeaterDefaultParams.SIGMA)
+
+        temperature = random.gauss(
+            PlasmaHeaterDefaultParams.TEMPERATURE, PlasmaHeaterDefaultParams.TEMPERATURE_SIGMA)
+
+        self.__model.temperature = temperature if temperature < PlasmaHeaterDefaultParams.TEMPERATURE else PlasmaHeaterDefaultParams.TEMPERATURE + \
+            random.random() * 10 ** -4
+
         self.__model.output_power_watt = random.gauss(
             PlasmaHeaterDefaultParams.OUTPUT_POWER, PlasmaHeaterDefaultParams.SIGMA)
         self.__model.input_current = random.gauss(
@@ -28,6 +33,9 @@ class DefaultGenerationStrategy(IGenerationStrategy):
             PlasmaHeaterDefaultParams.INPUT_VOLTAGE, PlasmaHeaterDefaultParams.SIGMA)
         self.__model.input_power = random.gauss(
             PlasmaHeaterDefaultParams.INPUT_POWER, PlasmaHeaterDefaultParams.SIGMA)
+        
+        if self.__model.durability <= 30:
+            self.__model.alarm = True
 
     def name(self) -> str:
         return self.__name
