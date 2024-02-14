70414d8de13af7c5b694d06acd3a5adff7398d4b
patyakm <devius_swag@mail.ru>
2024-02-14 14:23:45 +0700

    feat: add pressure stabilization unit architecture

---
+++ atmosphere_unit_service/service/domain/pressure_stabilization/pressure_stabilization.py
@@ -0,0 +1,38 @@
+class PressureStabilization:
+
+    def __init__(self, name: str = "pressure_stabilization") -> None:
+        self.name = name
+
+        self.is_on = False
+        self.alarm = False
+        self.durability = 100.0
+
+        self.sector = ""
+        self.is_suitable = False
+        self.time = 0.0
+
+        self.pressure = 750.0
+        self.CO2_volume = 0.02
+        self.O2_volume = 20
+        self.N2_volume = 78
+        self.other_volume = 1.98
+
+        self.speed = 50.0
+    def start(self) -> None:
+        self.is_on = True
+
+    is_on: bool
+    alarm: bool
+    durability: float
+
+    sector: str
+    is_suitable: bool
+    time = float
+
+    pressure: float
+    CO2_volume: float
+    O2_volume: float
+    N2_volume: float
+    other_volume: float
+
+    speed: float
