ee713c89a295fa6fd8b56ec66701a1060f14d1ed
patyakm <devius_swag@mail.ru>
2024-02-15 16:52:51 +0700

    fix: rename indexes feat: add HumidityManager.is_on attribute

--- atmosphere_unit_service/service/domain/device/device.py
+++ atmosphere_unit_service/service/domain/device/device.py
@@ -1,5 +1,6 @@
-from service.domain.pressure_stabilization.pressure_stabilization import PressureStabilization
-from service.domain.temperature_stabilization.temperature_stabilization import TemperatureStabilization
+from service.domain.pressure_stabilizer.pressure_stabilizer import PressureStabilizer
+from service.domain.temperature_stabilizer.temperature_stabilizer import TemperatureStabilizer
+from service.domain.humidity_manager.humidity_manager import HumidityManager
 
 class Device:
 
@@ -11,8 +12,9 @@ class Device:
         self.durability = 100.0
 
         self.sector = ""
-        PressureStabilization.is_on = False
-        TemperatureStabilization.is_on = False
+        PressureStabilizer.is_on = False
+        TemperatureStabilizer.is_on = False
+        HumidityManager.is_on = False
 
     is_on: bool
     alarm: bool
