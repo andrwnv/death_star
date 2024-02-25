1e9c0343ccaee904d1ef9b0d9513f15f74d26f39
PatyaKM <devius_swag@mail.ru>
2024-02-26 01:32:44 +0700

    feat: add atmosphere unit model

---
+++ atmosphere_unit_service/service/domain/model.py
@@ -0,0 +1,21 @@
+from  service.domain.device import Device
+
+class Model:
+    def __init__(self) -> None:
+        self.device= {
+            "sector 1": Device("sector 1"),
+            "sector 2": Device("sector 2"),
+            "sector 3": Device("sector 3"),
+            "sector 4": Device("sector 4"),
+            "sector 5": Device("sector 5"),
+            "sector 6": Device("sector 6"),
+            "sector 7": Device("sector 7"),
+            "sector 8": Device("sector 8"),
+            "sector 9": Device("sector 9"),
+            "sector 10": Device("sector 10"),
+            "sector 11": Device("sector 11"),
+            "sector 12": Device("sector 12"),
+            "sector 13": Device("sector 13"),
+            "sector 14": Device("sector 14"),
+            "sector 15": Device("sector 15"),
+        }
