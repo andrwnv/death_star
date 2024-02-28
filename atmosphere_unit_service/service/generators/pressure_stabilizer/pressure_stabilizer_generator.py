226ed19bb43b475ccd577e0e9a57fa4dbcf0371e
PatyaKM <devius_swag@mail.ru>
2024-02-28 18:08:44 +0700

    refactor: code simplification feat: add generation logic fix: correction of values for the Gauss's distribution

--- atmosphere_unit_service/service/generators/pressure_stabilizer/pressure_stabilizer_generator.py
+++ atmosphere_unit_service/service/generators/pressure_stabilizer/pressure_stabilizer_generator.py
@@ -10,13 +10,13 @@ from service.generators.generation_strategy import IGenerationStrategy
 class DefaultGenerationStrategy(IGenerationStrategy):
     @dataclass
     class GenerationParams:
-        PRESSURE = 750
+        PRESSURE = 750.0
         CO2_VOLUME = 1.5
         O2_VOLUME = 20
         N2_VOLUME = 78
         OTHER_VOLUME = 0.5
 
-        SIGMA = 2.0
+        SIGMA = 1.0
     
     def __init__(
         self,
@@ -51,16 +51,32 @@ class DefaultGenerationStrategy(IGenerationStrategy):
                 self.__model.O2_volume,
                 self.__model.N2_volume
             )
-        else:
-            pass
         
-        if self.__model.CO2_volume <= 3.0:
-            self.__model.is_suitable = True
-        
-        self.__model.alarm = False
+        self.__model.is_suitable = self.__model.CO2_volume <= 3.0
 
-        if self.__model.durability <= 20:
-            self.__model.alarm = True
+        self.__model.alarm = self.__model.durability <= 20
+        
+        if self.__model.durability <= 30:
+            while self.__model.repair_time != 0:
+                self.__model.pressure = 0.1 * (100/self.__model.durability)
+                self.__model.repair_time -= 1
+        else:
+            self.__model.pressure = 0.1 * (100/self.__model.durability) / 0.01
+            self.__model.CO2_volume += 1
+        
+        self.__model.is_on = not self.__model.CO2_volume >= 5
+        
+        if self.__model.is_on and self.__model.pressure != 750:
+            if self.__model.pressure > 750:
+                self.__model.pressure -= 50/60
+                self.__model.stabilizing_time += 1/60
+            else:
+                self.__model.pressure += 50/60
+                self.__model.stabilizing_time -= 1/60
+
+        if self.__model.alarm:
+            self.__model.is_on = False
+        
     def name(self) -> str:
         return self.__name
 
