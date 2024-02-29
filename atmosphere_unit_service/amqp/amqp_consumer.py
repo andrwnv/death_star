de54124e111c05e8b877fece13d2a86b7fb666ff
PatyaKM <devius_swag@mail.ru>
2024-02-29 17:58:13 +0700

    feat: add files and lines for starting service

---
+++ atmosphere_unit_service/amqp/amqp_consumer.py
@@ -0,0 +1,100 @@
+import logging
+
+from abc import ABC, abstractmethod
+from typing import Dict, List
+
+logger = logging.getLogger(__name__)
+
+logging.getLogger("pika").setLevel(logging.WARNING)
+
+
+class IMessageConsumer(ABC):
+
+    def __init__(self) -> None:
+        raise NotImplemented
+
+    @abstractmethod
+    def consume(self, data: Dict[str, any]) -> None:
+        raise NotImplemented
+
+
+class AmqpConsumer(ABC):
+    @abstractmethod
+    def add_consumer(self, consumer: IMessageConsumer):
+        raise NotImplemented
+
+    @abstractmethod
+    async def run(
+        self, addr: str, port: str, username: str, password: str, queue_name: str
+    ):
+        raise NotImplemented
+
+    @abstractmethod
+    def stop(self):
+        raise NotImplemented
+
+
+class RmqConsumer(AmqpConsumer):
+    def __init__(self) -> None:
+        self.__consumers: List[IMessageConsumer] = []
+        self.__is_started = False
+
+    def add_consumer(self, consumer: IMessageConsumer):
+        self.__consumers.append(consumer)
+
+    async def run(
+        self, addr: str, port: str, username: str, password: str, queue_name: str
+    ):
+        import pika
+
+        credentials = pika.PlainCredentials(
+            username=username,
+            password=password,
+        )
+
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(
+                host=addr,
+                port=port,
+                credentials=credentials,
+                socket_timeout=None,
+            )
+        )
+
+        self.__queue_name = queue_name
+
+        self.__channel = connection.channel()
+        self.__channel.queue_declare(queue=self.__queue_name, durable=True)
+
+        self.__is_started = True
+
+        while self.__is_started:
+            import asyncio
+
+            method_frame, properties, body = self.__channel.basic_get(self.__queue_name)
+            if method_frame is not None and properties is not None and body is not None:
+                if self.__consume(body):
+                    self.__channel.basic_ack(method_frame.delivery_tag)
+
+            await asyncio.sleep(0.1)
+
+    def __consume(self, body) -> bool:
+        try:
+            import json
+
+            message = json.loads(body)
+            for consumer in self.__consumers:
+                consumer.consume(message)
+
+            logger.debug(f"Got message from queue! Message={message}")
+
+            return True
+        except Exception:
+            logger.error(
+                f"Got unknown message type! Failed to parse it! Message={body}"
+            )
+
+        return False
+
+    def stop(self):
+        self.__channel.stop_consuming()
---
+++ atmosphere_unit_service/service/config.py
@@ -0,0 +1,74 @@
+from dataclasses import dataclass
+
+@dataclass
+class AmpqProps:
+    addr: str
+    port: str
+    username: str
+    password: str
+    queue_name: str
+
+@dataclass
+class SelfProps:
+    grpc_addr: str
+    grpc_port: str
+
+    event_queue_props: AmpqProps
+
+@dataclass
+class ScenaristProps:
+    event_queue_props: AmpqProps
+
+class Config:
+
+    def __init__(self) -> None:
+        self.self._props = SelfProps(
+            grpc_addr="0.0.0.0",
+            grpc_port=4040,
+            event_queue_props=AmpqProps(
+                addr="0.0.0.0",
+                port=5560,
+                username="guest",
+                password="guest",
+                queue_name="scenarist_action_queue",
+            ),
+        )
+        self.scenarist_props = ScenaristProps(
+            event_queue_props=AmpqProps(
+                addr="0.0.0.0",
+                port=5560,
+                username="guest",
+                password="guest",
+                queue_name="atmosphere_unit_action_queue",
+            ),
+        )
+    
+    def init_with_file(self, file: str):
+        import yaml
+
+        with open(file, "r") as file:
+            cfg = yaml.safe_load(file)
+
+            self.self_props.grpc_addr = cfg["grpc_addr"]
+            self.self_props.grpc_port = cfg["grpc_port"]
+            self.self_props.event_queue_props.addr = cfg["event_queue_addr"]
+            self.self_props.event_queue_props.port = cfg["event_queue_port"]
+            self.self_props.event_queue_props.username = cfg["event_queue_username"]
+            self.self_props.event_queue_props.password = cfg["event_queue_password"]
+            self.self_props.event_queue_props.queue_name = cfg["event_queue_name"]
+
+            self.scenarist_props.event_queue_props.addr = cfg["scenarist"][
+                "event_queue_addr"
+            ]
+            self.scenarist_props.event_queue_props.port = cfg["scenarist"][
+                "event_queue_port"
+            ]
+            self.scenarist_props.event_queue_props.username = cfg["scenarist"][
+                "event_queue_username"
+            ]
+            self.scenarist_props.event_queue_props.password = cfg["scenarist"][
+                "event_queue_password"
+            ]
+            self.self_props.event_queue_props.queue_name = cfg["scenarist"][
+                "event_queue_name"
+            ]
